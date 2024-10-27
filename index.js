d87e5e7c77eb96f2155dcbd93c87e9dfbf17458d
taydeo <td12233a@gmail.com>
2024-10-26 20:54:29 -0500

    updated website to include MRP

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const funkyStationUrl = "https://status.funkystation.org/status"
+const funkyStationUrl = "https://status.funkystation.org/lrp/status"
+const funkyStationMrpUrl = "https://status.funkystation.org/mrp/status"
 
 async function getData() {
     try {
@@ -14,11 +15,32 @@ async function getData() {
       console.error(error.message);
     }
 }
-  
+ 
+async function getMrpData() {
+  try {
+    const response = await fetch(funkyStationMrpUrl);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
 
 var res = await getData()
+var mrpRes = await getMrpData()
 
 document.getElementById('playersOnFunky').innerText = res.players
 document.getElementById('funkystationName').innerText = res.name
 document.getElementById('gamePreset').innerText = res.preset
 document.getElementById('gameMap').innerText = res.map ? res.map : 'Lobby'
+
+document.getElementById('playersOnFunkyMrp').innerText = mrpRes.players
+document.getElementById('funkystationmrpname').innerText = mrpRes.name
+document.getElementById('gamePresetMrp').innerText = mrpRes.preset
+document.getElementById('gameMapMrp').innerText = mrpRes.map ? mrpRes.map : 'Lobby'
